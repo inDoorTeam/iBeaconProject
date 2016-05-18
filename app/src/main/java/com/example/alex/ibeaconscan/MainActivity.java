a4cbe1c2f325adb8b36c8ebabb4bd23357adc0b6
x2413915 <x2413915@gmail.com>
2016-05-19 05:21:47 +0800

    version2.2

--- app/src/main/java/com/example/alex/ibeaconscan/MainActivity.java
+++ app/src/main/java/com/example/alex/ibeaconscan/MainActivity.java
@@ -1,18 +1,11 @@
 package com.example.alex.ibeaconscan;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
-import android.content.Context;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import org.altbeacon.beacon.*;
-
-import java.net.InetSocketAddress;
 import java.util.Collection;
 
 import java.io.DataOutputStream;
@@ -21,11 +14,8 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer{
-    protected static final String TAG = "MainActivity";
     private BeaconManager beaconManager;
 
-    private BluetoothManager BTManager;
-    private BluetoothAdapter BTAdapter = null;
     private TextView RssiText,UuidText,MajorText,MinorText;
     private Handler mHandler;
     private int Rssi, Major, Minor;
@@ -54,10 +44,6 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
         beaconManager.bind(this);
 
         mHandler = new Handler();
-        //BTAdapter = BluetoothAdapter.getDefaultAdapter();
-        //BTManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
-        //mHandler.post(scanRunnable);
-
     }
 
     @Override
@@ -121,65 +107,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
                 }
             }
 
-            //BTAdapter.startLeScan(leScanCallback);
-            //mHandler.postDelayed(this, 2000);
-            //BTAdapter.stopLeScan(leScanCallback);
         }
     };
-/*
-    public BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback()
-    {
-        @Override
-        public void onLeScan(final BluetoothDevice device, int Rssi, byte[] scanRecord)
-        {
-            int startByte = 2;
-            boolean patternFound = false;
-            while (startByte <= 5)
-            {
-                if (    ((int) scanRecord[startByte + 2] & 0xff) == 0x02 &&
-                        ((int) scanRecord[startByte + 3] & 0xff) == 0x15)
-                {
-                    patternFound = true;
-                    break;
-                }
-                startByte++;
-            }
 
-            if (patternFound)
-            {
-                //Convert to hex String
-                byte[] uuidBytes = new byte[16];
-                System.arraycopy(scanRecord, startByte + 4, uuidBytes, 0, 16);
-                String hexString = bytesToHex(uuidBytes);
-
-                //UUID detection
-                String Uuid =  hexString.substring(0,8) + "-" +
-                        hexString.substring(8,12) + "-" +
-                        hexString.substring(12,16) + "-" +
-                        hexString.substring(16,20) + "-" +
-                        hexString.substring(20,32);
-
-                int Major = (scanRecord[startByte + 20] & 0xff) * 0x100 + (scanRecord[startByte + 21] & 0xff);
-                int Minor = (scanRecord[startByte + 22] & 0xff) * 0x100 + (scanRecord[startByte + 23] & 0xff);
-                RssiText.setText(Rssi + "");
-                UuidText.setText(Uuid);
-                MajorText.setText(Major + "");
-                MinorText.setText(Minor + "");
-            }
-            BTAdapter.stopLeScan(leScanCallback);
-        }
-
-    };
-    static final char[] hexArray = "0123456789ABCDEF".toCharArray();
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ )
-        {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-    */
 }
